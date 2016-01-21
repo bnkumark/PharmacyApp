f8bae3324a3ebf22d91d55190b019414b3ef79f0
bnkumark <bnkumar_k@yahoo.co.in>
2016-01-21 23:25:49 +0530

    integration with web services

--- www/js/home.js
+++ www/js/home.js
@@ -30,5 +30,38 @@ var myApp = angular.module('homeModule', [])
         console.log($scope.ordercollections.length);
     };
 
-    
+    $scope.UpdateStatus = function (deliveryType, order)
+    {
+        var storeId = 1;
+        var orderItems = "{";
+        var seperator = "";
+        for (i = 0; i < order.orderItemInfo.length; i++) {
+            console.log('order:' + order)
+            console.log('order.orderItemInfo:' + order.orderItemInfo)
+            console.log('order.orderItemInfo[i]:' + order.orderItemInfo[i])
+            var availabilityIndex=-1;
+            if (order.orderItemInfo[i].availabilityIndex == true)
+            {
+                availabilityIndex = 1;
+            }
+            if (i < order.orderItemInfo.length)
+            {
+                seperator = ",";
+        }
+            orderItems = orderItems + "\"" + order.orderItemInfo[i].brandId + "\":" + availabilityIndex + seperator;
+            
+        }
+        orderItems = orderItems + "}";
+        console.log('orderItems: '+orderItems)
+
+        $http.get($config.serverUrl + "webservice/updateAvailabilityFromNonPartners?storeId=" + storeId + "&orderItems=" + orderItems + "&deliveryType=" + deliveryType + "&collId=" + order.collectionId)  //FIXME : Store id should be dynamically fetched
+         .success(function (data) {
+             $scope.IgnoreOrder(order);
+             console.log('updateAvailabilityFromNonPartners success:' + data);
+         })
+         .error(function (data) {
+             $scope.checkingForOrders = false;
+             $CheckNetwork.check();
+         });
+    }
 }])
